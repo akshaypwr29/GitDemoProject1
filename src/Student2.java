a409e0b2fda853a635dbeaa730a54a34dd022c23
AkshayPawar <AkshayPawar@192.168.1.162>
2024-01-22 15:14:59 +0530

    Commiting Test2to BranchX

---
+++ src/Student2.java
@@ -0,0 +1,20 @@
+
+public class Student2 {
+	
+	String name;
+	int rno;
+	
+	Student2()
+	{
+		name = "ABC";
+		rno = 123;
+	}
+
+	public static void main(String[] args) 
+	{
+		Student2 s1= new Student2();
+		System.out.println(s1.name);
+		System.out.println(s1.rno);
+	}
+
+}
