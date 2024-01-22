be4e8b534585cf06cff405a6520003da9db312ae
AkshayPawar <AkshayPawar@192.168.1.162>
2024-01-22 15:54:17 +0530

    New Student is added

--- src/Student2.java
+++ src/Student2.java
@@ -1,13 +1,16 @@
 
 public class Student2 {
 	
-	String name;
-	int rno;
+	String name, name1;
+	int rno, rno1;
 	
 	Student2()
 	{
 		name = "ABC";
 		rno = 123;
+		
+		name1 = "xyz";
+		rno1 = 456;
 	}
 
 	public static void main(String[] args) 
@@ -15,6 +18,10 @@ public class Student2 {
 		Student2 s1= new Student2();
 		System.out.println(s1.name);
 		System.out.println(s1.rno);
+		
+		Student2 s2= new Student2();
+		System.out.println(s2.name1);
+		System.out.println(s2.rno1);
 	}
 
 }
